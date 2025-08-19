288786a6091037ad1894451ff878bdf667a2aa48
AcronicalYT <ace36501@hotmail.com>
2025-08-20 00:53:44 +0100

    OS checks

--- js/launcher.js
+++ js/launcher.js
@@ -1,3 +1,5 @@
+const platforms = ['windows', 'macos', 'linux'];
+
 async function fetchLauncherInfo() {
     const apiUrl = 'https://api.teli.games/launcher/latest';
 
@@ -17,14 +19,30 @@ async function fetchLauncherInfo() {
 
 function renderLauncherButton(data) {
     const launcherContainer = document.getElementById('launcher-container');
+    let platform = 'unknown';
 
-    if (!data || !data.downloadUrl || !data.version) {
+    if (!data || !data.platforms || !data.version) {
         launcherContainer.innerHTML = `<p class="text-red-500">Could not fetch launcher info. Please try again later.</p>`;
         return;
     }
 
+    const userPlatform = navigator.platform.toLowerCase();
+
+    if (userPlatform.startsWith('win')) platform = 'windows-x86_64';
+    else if (userPlatform.startsWith('mac')) platform = userPlatform.includes('macintel') ? 'macintel' : 'macarm';
+    else if (userPlatform.startsWith('linux')) platform = 'linux-x86_64';
+    else platform = 'unknown';
+
+
+    if (!data.platforms[platform]) {
+        launcherContainer.innerHTML = `<p class="text-red-500">Your platform (${[platform]}) is not supported by the launcher.</p>`;
+        return;
+    }
+
+    const downloadUrl = data.platforms[platform]?.url || null;
+
     launcherContainer.innerHTML = `
-        <a href="${data.downloadUrl}" target="_blank" rel="noopener noreferrer" class="inline-block bg-light-primary text-light-bg dark:bg-dark-primary dark:text-dark-bg font-pixel text-lg px-8 py-4 rounded-md pixel-shadow hover:scale-105 transition-transform">
+        <a href="${downloadUrl}" target="_blank" rel="noopener noreferrer" class="inline-block bg-light-primary text-light-bg dark:bg-dark-primary dark:text-dark-bg font-pixel text-lg px-8 py-4 rounded-md pixel-shadow hover:scale-105 transition-transform">
             Download v${data.version}
         </a>
     `;
